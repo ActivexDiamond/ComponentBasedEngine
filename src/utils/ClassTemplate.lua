616769e4d9beabe850e9df666b1cc4d7c01a8628
ActivexDiamond <activexdiamond@gmail.com>
2020-01-18 10:55:37 +0300

    Began work on weapon system.

--- src/utils/ClassTemplate.lua
+++ src/utils/ClassTemplate.lua
@@ -1,3 +1,4 @@
+------------------------------ Obj + Super ------------------------------
 local class = require "libs.cruxclass"
 local Super = require "x"
 
@@ -8,12 +9,22 @@ end
 
 return Object
 
------------------------
-
+------------------------------ Obj ------------------------------
 local class = require "libs.cruxclass"
 
 local Object = class("Unnamed")
 function Object:init()
+end
+
+return Object
+
+------------------------------ Obj + Thing ------------------------------
+local class = require "libs.cruxclass"
+local Thing = require "template.Thing"
+
+local Object = class("Unnamed", Thing)
+function Object:init(id)
+	Thing.init(self, id)
 end
 
 return Object
